4781b23f51468dad38f062118039b00a5aaa7c77
Glenn <a810.glenn@gmail.com>
2021-08-29 22:47:22 +0900

    state 사용하기

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,16 +1,22 @@
-import React from 'react'
-import logo from './logo.svg'
+import React, { useState } from 'react'
+// import logo from './logo.svg'
 import './App.css';
 
 function App() {
-  let posts = '강남 고기 맛집'
+  const [title, newTitle] = useState('남자 코트 추천')
+
+  // const posts = '강남 고기 맛집'
 
   return (
     <div className="App">
       <div className='black-nav'>
         <div>개발 Blog</div>
       </div>
-      <p>{posts}</p>
+      <div className='list'>
+        <h3>{title}</h3>
+        <p>2월 17일 발행</p>
+        <hr />
+      </div>
     </div>
   );
 }
