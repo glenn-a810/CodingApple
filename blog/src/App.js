8817b31c229ae218690c02cd1d6a3ac5c99b0feb
Glenn <a810.glenn@gmail.com>
2021-09-01 22:38:22 +0900

    변경 array 주석처리

--- blog/src/App.js
+++ blog/src/App.js
@@ -6,18 +6,18 @@ function App() {
   const [title, newTitle] = useState(['남자코트 추천', '강남 우동맛집', '리액트 독학'])
   const [likeCount, newLikeCount] = useState(0)
   // const posts = '강남 고기 맛집'
-  const titleChange = () => {
-    let titles = [...title]
-    titles[0] = '여자코트 추천'
-    newTitle(titles)
-  }
+  // const titleChange = () => {
+  //   let titles = [...title]
+  //   titles[0] = '여자코트 추천'
+  //   newTitle(titles)
+  // }
 
   return (
     <div className="App">
       <div className='black-nav'>
         <div>개발 Blog</div>
       </div>
-      <button onClick={titleChange} >제목변경</button>
+      {/* <button onClick={titleChange} >제목변경</button> */}
       <div className='list'>
         <h3>{title[0]} <span onClick={() => { newLikeCount(likeCount+1) }} >👍</span> {likeCount} </h3>
         <p>2월 17일 발행</p>
