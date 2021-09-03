16a4fc4fdcafce9def74b38374e2af19bca7af57
Glenn <a810.glenn@gmail.com>
2021-09-03 23:05:24 +0900

    데이터 바인딩 :는 속성 {{}}는 값. data()에 obj형태로 일단 다 때려박자.

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,16 +1,45 @@
 <template>
-  <div id="app">
+  <!-- <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view />
+  </div> -->
+  <div id="app">
+    <div>
+      <h4>{{ products[0] }}</h4>
+      <p>{{ price1 }} 만원</p>
+    </div>
+    <div>
+      <h4>{{ products[1] }}</h4>
+      <p>{{ price2 }} 만원</p>
+    </div>
+    <div>
+      <h4>{{ products[2] }}</h4>
+      <p>{{ price3 }} 만원</p>
+    </div>
   </div>
 </template>
 
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      price1: '1000/120',
+      price2: '1000/130',
+      price3: '1000/110',
+      products: ['역삼동원룸', '삼평동원룸', '백현동원룸']
+    }
+  },
+}
+</script>
+
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif; */
+  font-family: -apple-system, BlinkMacSystemFont, 'Apple SD Gothic Neo', Pretendard, Roboto, 'Noto Sans KR', 'Segoe UI', 'Malgun Gothic', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
