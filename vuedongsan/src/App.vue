6163905326085add0c3e1e201bf20ab3ecc480c4
Glenn <a810.glenn@gmail.com>
2021-09-04 00:06:06 +0900

    v-for 활용법. key에 대한 기초 설명을 더 봐야함.

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -7,7 +7,20 @@
     <router-view />
   </div> -->
   <div id="app">
-    <div>
+    <div class="menu">
+      <a v-for="(topMenu, menuId) in menus" :key="menuId">{{ topMenu }}</a>
+    </div>
+    <!-- <div>
+      <h4 v-for="(contents, contentsId) in products" :key="contentsId">
+        {{ contents }}
+      </h4>
+      <p v-for="(price, priceId) in price" :key="priceId">{{ price }}</p>
+    </div> -->
+    <div v-for="(contents, contentsId) in products" :key="contentsId">
+      <h4>{{contents}}</h4>
+      <p>{{price[contentsId]}}</p>
+    </div>
+    <!-- <div>
       <h4>{{ products[0] }}</h4>
       <p>{{ price1 }} 만원</p>
     </div>
@@ -18,7 +31,7 @@
     <div>
       <h4>{{ products[2] }}</h4>
       <p>{{ price3 }} 만원</p>
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -27,10 +40,9 @@ export default {
   name: 'App',
   data() {
     return {
-      price1: '1000/120',
-      price2: '1000/130',
-      price3: '1000/110',
-      products: ['역삼동원룸', '삼평동원룸', '백현동원룸']
+      price: ['1000/120', '1000/130', '1000/110'],
+      products: ['역삼동원룸', '삼평동원룸', '백현동원룸'],
+      menus: ['Home', 'Shop', 'About'],
     }
   },
 }
@@ -39,7 +51,8 @@ export default {
 <style>
 #app {
   /* font-family: Avenir, Helvetica, Arial, sans-serif; */
-  font-family: -apple-system, BlinkMacSystemFont, 'Apple SD Gothic Neo', Pretendard, Roboto, 'Noto Sans KR', 'Segoe UI', 'Malgun Gothic', sans-serif;
+  font-family: -apple-system, BlinkMacSystemFont, 'Apple SD Gothic Neo',
+    Pretendard, Roboto, 'Noto Sans KR', 'Segoe UI', 'Malgun Gothic', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -57,5 +70,16 @@ export default {
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+.menu {
+  background: #2c3e50;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
