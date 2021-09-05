10394fa741589205e0bac4db037fd1fcd0d80f19
Glenn <a810.glenn@gmail.com>
2021-09-05 15:49:14 +0900

    arr 리렌더링은 일단 forceUpdate()로 땜빵. 모달창 만들기 연습

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -6,7 +6,18 @@
     </div>
     <router-view />
   </div> -->
+
   <div id="app">
+    <!-- 모달 -->
+    <div class="black-bg" v-if="modalOpen === true">
+      <div class="white-bg">
+        <h4>상세페이지</h4>
+        <p>상세페이지 내용</p>
+        <button @click="modalOpen = false">닫기</button>
+      </div>
+    </div>
+
+    <!-- 메뉴 -->
     <div class="menu">
       <a v-for="(topMenu, menuId) in menus" :key="menuId">{{ topMenu }}</a>
     </div>
@@ -14,22 +25,27 @@
       <h4>{{contents}}</h4>
       <p>{{price[contentsId]}}</p>
     </div> -->
+
+    <!-- 내용 -->
     <div>
-      <h4>{{ products[0] }}</h4>
+      <img src="./assets/room0.jpg" class="room-img">
+      <h4 @click="modalOpen = true">{{ products[0] }}</h4>
       <p>{{ price[0] }} 만원</p>
-      <button @click="fakeCopy">허위매물신고</button>
+      <button @click="[fake[0]++, $forceUpdate()]">허위매물신고</button>
       <span>신고수 : {{ fake[0] }}</span>
     </div>
     <div>
-      <h4>{{ products[1] }}</h4>
+      <img src="./assets/room1.jpg" class="room-img">
+      <h4 @click="modalOpen = true">{{ products[1] }}</h4>
       <p>{{ price[1] }} 만원</p>
-      <button @click="fake[1]++">허위매물신고</button>
+      <button @click="[fake[1]++, $forceUpdate()]">허위매물신고</button>
       <span>신고수 : {{ fake[1] }}</span>
     </div>
     <div>
-      <h4>{{ products[2] }}</h4>
+      <img src="./assets/room2.jpg" class="room-img">
+      <h4 @click="modalOpen = true">{{ products[2] }}</h4>
       <p>{{ price[2] }} 만원</p>
-      <button @click="fake[2]++">허위매물신고</button>
+      <button @click="[fake[2]++, $forceUpdate()]">허위매물신고</button>
       <span>신고수 : {{ fake[2] }}</span>
     </div>
   </div>
@@ -40,6 +56,7 @@ export default {
   name: 'App',
   data() {
     return {
+      modalOpen: false,
       fake: [0, 0, 0],
       price: ['1000/120', '1000/130', '1000/110'],
       products: ['역삼동원룸', '삼평동원룸', '백현동원룸'],
@@ -52,12 +69,40 @@ export default {
       const copyFake = [...this.fake]
       copyFake[0]++
       this.fake = copyFake
-    }
-  }
+    },
+  },
 }
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed;
+  padding: 20px;
+}
+
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
+
 #app {
   /* font-family: Avenir, Helvetica, Arial, sans-serif; */
   font-family: -apple-system, BlinkMacSystemFont, 'Apple SD Gothic Neo',
