5eeea76f66900e88664dbe3a136d4e3f5ce8d663
Glenn <a810.glenn@gmail.com>
2021-09-06 23:16:00 +0900

    watcher 사용법

--- vuedongsan/src/components/Modal.vue
+++ vuedongsan/src/components/Modal.vue
@@ -1,33 +1,39 @@
 <template>
-    <!-- 모달 -->
-    <div class="black-bg" v-if="modalOpen === true">
-        <div class="white-bg">
-            <img :src="products[userSelect].image" class="room-img" />
-            <h4>{{ products[userSelect].title }}</h4>
-            <p>{{ products[userSelect].content }}</p>
-            <input v-model.number="month">
-            <p>{{month}}개월 선택 : {{ products[userSelect].price * month }}원</p>
-            <button @click="$emit('closeModal')">닫기</button>
-        </div>
+  <!-- 모달 -->
+  <div class="black-bg" v-if="modalOpen === true">
+    <div class="white-bg">
+      <img :src="products[userSelect].image" class="room-img" />
+      <h4>{{ products[userSelect].title }}</h4>
+      <p>{{ products[userSelect].content }}</p>
+      <input v-model.number="month" />
+      <p>{{ month }}개월 선택 : {{ products[userSelect].price * month }}원</p>
+      <button @click="$emit('closeModal')">닫기</button>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Modal',
-    data() {
-        return {
-            month: 1,
-        }
-    },
-    props: {
-        products: Array,
-        modalOpen: Boolean,
-        userSelect: Number,
+  name: 'Modal',
+  data() {
+    return {
+      month: 1,
     }
+  },
+  watch: {
+    month(userInput) {
+      if (isNaN(userInput) == true) {
+        alert('숫자만 입력할 수 있습니다.')
+        this.month = 1
+      }
+    },
+  },
+  props: {
+    products: Array,
+    modalOpen: Boolean,
+    userSelect: Number,
+  },
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
