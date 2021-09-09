009d1cbb671ad3508517e8d46ef91e7e735794db
Glenn <a810.glenn@gmail.com>
2021-09-10 04:17:26 +0900

    샘플 데이터 props로 값 넘겨서 화면 출력

---
+++ vuestagram/src/components/container.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <post :postData="postData" />
+    <post />
+    <post />
+  </div>
+</template>
+
+<script>
+import post from './post.vue';
+
+export default {
+  name: 'container',
+  data() {
+    return {
+      
+    }
+  },
+  components: {
+    post,
+  },
+  props: {
+    postData: Array,
+  }
+};
+</script>
+
+<style></style>
---
+++ vuestagram/src/components/post.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="post">
+    <div class="post-header">
+      <div class="profile"></div>
+      <span class="profile-name">{{ postData[0].name }}</span>
+    </div>
+    <div class="post-body"></div>
+    <div class="post-content">
+      <p>{{ postData[0].likes }} Likes</p>
+      <p>
+        <strong>{{ postData[0].filter }}</strong> {{ postData[0].content }}
+      </p>
+      <p class="date">{{postData[0].date}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'post',
+  data() {
+    return {};
+  },
+  props: {
+    postData: Array,
+  },
+};
+</script>
+
+<style>
+.post {
+  width: 100%;
+}
+.profile {
+  background-image: url('https://placeimg.com/100/100/arch');
+  width: 30px;
+  height: 30px;
+  background-size: 100%;
+  border-radius: 50%;
+  float: left;
+}
+.profile-name {
+  display: block;
+  float: left;
+  padding-left: 10px;
+  padding-top: 7px;
+  font-size: 14px;
+}
+.post-header {
+  height: 30px;
+  padding: 10px;
+}
+.post-body {
+  background-image: url('https://placeimg.com/640/480/animals');
+  height: 450px;
+  background-position: center;
+  background-size: cover;
+}
+.post-content {
+  padding-left: 15px;
+  padding-right: 15px;
+  font-size: 14px;
+}
+.date {
+  font-size: 11px;
+  color: grey;
+  margin-top: -8px;
+}
+</style>
