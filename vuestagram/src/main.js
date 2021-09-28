4a895c130e0a32e991cfda22e054daceff599cf2
Glenn <a810.glenn@gmail.com>
2021-09-28 21:36:03 +0900

    mitt 자체가 3.x에서 사용하는 것

--- vuestagram/src/main.js
+++ vuestagram/src/main.js
@@ -2,9 +2,6 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-// import mitt from 'mitt';
-
-// const emitter = mitt();
 
 Vue.config.productionTip = false;
 
