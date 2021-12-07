740abe59fb457a2480503eaf4492d7b20d724419
Glenn <a810.glenn@gmail.com>
2021-12-07 21:44:52 +0900

    HTML태그안의 속성데이터 바인딩은 ':속성', 내용 데이터바인딩은 '{{데이터}}'

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -13,46 +13,48 @@
     </a>
   </div>
 
-  <div>
-    <img src="./assets/room0.jpg" alt="room0" class="room-img" />
-    <h4 @click="modalState = true">{{ products[0] }}</h4>
-    <p>{{ price[0] }}만원</p>
-    <button @click="increment(0)">허위매물신고</button>
-    <span> 신고수 : {{ reportCount[0] }}</span>
-  </div>
-  <div>
-    <img src="./assets/room1.jpg" alt="room1" class="room-img" />
-    <h4 @click="modalState = true">{{ products[1] }}</h4>
-    <p>{{ price[1] }}만원</p>
-    <button @click="increment(1)">허위매물신고</button>
-    <span> 신고수 : {{ reportCount[1] }}</span>
-  </div>
-  <div>
-    <img src="./assets/room2.jpg" alt="room2" class="room-img" />
-    <h4 @click="modalState = true">{{ products[2] }}</h4>
-    <p>{{ price[2] }}만원</p>
-    <button @click="increment(2)">허위매물신고</button>
-    <span> 신고수 : {{ reportCount[2] }}</span>
-  </div>
+  <!--  <div>-->
+  <!--    <img src="./assets/room0.jpg" alt="room0" class="room-img" />-->
+  <!--    <h4 @click="modalState = true">{{ products[0] }}</h4>-->
+  <!--    <p>{{ price[0] }}만원</p>-->
+  <!--    <button @click="increment(0)">허위매물신고</button>-->
+  <!--    <span> 신고수 : {{ reportCount[0] }}</span>-->
+  <!--  </div>-->
+  <!--  <div>-->
+  <!--    <img src="./assets/room1.jpg" alt="room1" class="room-img" />-->
+  <!--    <h4 @click="modalState = true">{{ products[1] }}</h4>-->
+  <!--    <p>{{ price[1] }}만원</p>-->
+  <!--    <button @click="increment(1)">허위매물신고</button>-->
+  <!--    <span> 신고수 : {{ reportCount[1] }}</span>-->
+  <!--  </div>-->
+  <!--  <div>-->
+  <!--    <img src="./assets/room2.jpg" alt="room2" class="room-img" />-->
+  <!--    <h4 @click="modalState = true">{{ products[2] }}</h4>-->
+  <!--    <p>{{ price[2] }}만원</p>-->
+  <!--    <button @click="increment(2)">허위매물신고</button>-->
+  <!--    <span> 신고수 : {{ reportCount[2] }}</span>-->
+  <!--  </div>-->
 
-  <hr />
+  <!--  <hr />-->
 
-  <div v-for="(productsTitle, i) in products" :key="i">
-    <h4 @click="modalState = true">{{ productsTitle }}</h4>
-    <p>{{ price[i] }}만원</p>
-    <button @click="increment(i)">허위매물신고</button>
-    <span> 신고수 : {{ reportCount[i] }}</span>
+  <div v-for="rooms in products" :key="rooms.id">
+    <img :src="rooms.image" alt="image" class="room-img" />
+    <h4 @click="modalState = true">{{ rooms.title }}</h4>
+    <p>{{ rooms.price }}만원</p>
   </div>
 </template>
 
 <script>
+import roomData from "./assets/roomData";
+
 export default {
   name: "App",
   data: () => ({
-    products: ["삼평동 원룸", "백현동 원룸", "판교동 원룸"],
+    // products: ["삼평동 원룸", "백현동 원룸", "판교동 원룸"],
+    products: roomData,
     price: ["60", "70", "80"],
     menus: ["Home", "Products", "About"],
-    reportCount: [0, 0, 0],
+    reportCount: [0, 0, 0, 0, 0, 0],
     modalState: false,
   }),
   methods: {
