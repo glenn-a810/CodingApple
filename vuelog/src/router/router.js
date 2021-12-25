a035bc87987068b5dd1fa1bb95e9148c4ea8605c
Glenn <a810.glenn@gmail.com>
2021-12-25 22:04:21 +0900

    router hash mode에 대해. URL에 #이 붙은채로 시작되는 주소가 되는데 #뒤에 있는 내용들은 절대 서버로 전달되지 않음. 서버가 라우팅을 채가는 일을 방지하고 Vue router에게 온전히 라우팅을 맡길 수 있음. Navigation guards도 알아둘 것

--- vuelog/src/router/router.js
+++ vuelog/src/router/router.js
@@ -1,4 +1,4 @@
-import { createWebHistory, createRouter } from "vue-router";
+import { createWebHashHistory, createRouter } from "vue-router";
 import List from "@/components/List";
 import Home from "@/components/Home";
 import Detail from "@/components/Detail";
@@ -35,7 +35,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
