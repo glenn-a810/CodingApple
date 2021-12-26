faab8366933b95073de812a3b7b114dc3c85a770
Glenn <a810.glenn@gmail.com>
2021-12-26 21:23:10 +0900

    vuestagram 새 프로젝트 세팅 / layout 설정 / porps 테스트

---
+++ vuestagram/src/components/Container.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Post :postData="postData" />
+  </div>
+</template>
+
+<script>
+import Post from "@/components/Post";
+
+export default {
+  name: "Container",
+  components: {
+    Post,
+  },
+  props: {
+    postData: Array,
+  },
+};
+</script>
+
+<style></style>
