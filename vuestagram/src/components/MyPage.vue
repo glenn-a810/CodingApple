042f23aa0a2ddd342f2cbfeb485ff06a882867df
Glenn <a810.glenn@gmail.com>
2022-01-12 00:57:35 +0900

    composition API로 값 받아와서 뿌려보기

--- vuestagram/src/components/MyPage.vue
+++ vuestagram/src/components/MyPage.vue
@@ -2,9 +2,21 @@
   <div style="padding: 10px">
     <h4>팔로워</h4>
     <input placeholder="Search" />
-    <div class="post-header">
-      <div class="profile"></div>
-      <span class="profile-name">{{ follower }}</span>
+    <!--    <div class="post-header">-->
+    <!--      <div class="profile"></div>-->
+    <!--      <span class="profile-name">{{ follower[0].name }}</span>-->
+    <!--    </div>-->
+
+    <div
+      v-for="followerList in follower"
+      :key="followerList"
+      class="post-header"
+    >
+      <div
+        class="profile"
+        :style="{ backgroundImage: `url(${followerList.image})` }"
+      ></div>
+      <span class="profile-name">{{ followerList.name }}</span>
     </div>
   </div>
 </template>
