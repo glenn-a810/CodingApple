7ec4791654a765d333d4b9c3710da766fcc88c5f
Glenn <a810.glenn@gmail.com>
2022-01-12 22:19:51 +0900

    javascript 다 까먹은거 같으니 다시 해야게따

--- vuestagram/src/components/MyPage.vue
+++ vuestagram/src/components/MyPage.vue
@@ -1,11 +1,7 @@
 <template>
   <div style="padding: 10px">
     <h4>팔로워</h4>
-    <input placeholder="Search" />
-    <!--    <div class="post-header">-->
-    <!--      <div class="profile"></div>-->
-    <!--      <span class="profile-name">{{ follower[0].name }}</span>-->
-    <!--    </div>-->
+    <input @input="search($event.target.value)" placeholder="Search" />
 
     <div
       v-for="followerList in follower"
@@ -29,15 +25,25 @@ export default {
   name: "MyPage",
   setup() {
     let follower = ref([]);
+    let followerOrigin = ref([]);
 
     onMounted(() => {
       axios.get("/follower.json").then((res) => {
         follower.value = res.data;
+        followerOrigin.value = [...res.data]
       });
     });
 
+    function search(i) {
+      let matchResult = followerOrigin.value.filter((a) => {
+        return a.name.indexOf(i) !== -1
+      });
+      follower.value = [...matchResult]
+    }
+
     return {
       follower,
+      search,
     };
   },
 };
